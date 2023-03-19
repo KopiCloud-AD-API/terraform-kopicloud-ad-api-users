548a5f197b82f985d81dcb5249634ae5c233e6e2
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-03-19 21:03:50 +0100

    Initial commit

---
+++ user-create.tf
@@ -0,0 +1,24 @@
+#######################################
+## KopiCloud AD API - Create AD User ##
+#######################################
+
+# Create AD User
+resource "kopicloud_user" "test" {
+  username      = "rsmith"
+  password      = var.password
+  first_name    = "Robert"
+  initials      = "T"
+  last_name     = "Smith"
+  display_name  = "Robert T Smith"
+  company       = "KopiCloud Limited"
+  description   = "Marketing Director"
+  department    = "Marketing"
+  email_address = "robert.smith@kopicloud.net"
+}
+
+# Returns Create AD User Result
+output "OUTPUT_new_user" {
+  description = "Created User"
+  value       = resource.kopicloud_user.test
+  sensitive = true
+}
